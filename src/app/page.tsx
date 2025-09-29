43022c3a8af5dd6371f8828b394b02bd0c1ee222
Amandeep Singh <amandeep90s@users.noreply.github.com>
2025-09-29 16:21:51 +0530

    feat: Add Google provider to authentication and update Home component to display sign-out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { useAuthActions } from '@convex-dev/auth/react';
+
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return 'Home Screen';
+  const { signOut } = useAuthActions();
+
+  return (
+    <div>
+      Logged In
+      <Button type="button" onClick={signOut}>
+        Signout
+      </Button>
+    </div>
+  );
 }
